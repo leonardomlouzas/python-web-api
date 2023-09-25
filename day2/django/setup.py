e432ec41a3797ce68211849a512fd7ea27cdff7b
leomlouzas <leonardomlouzas@gmail.com>
2023-09-25 12:59:05 -0300

    Configures app for deploy

--- day2/django/setup.py
+++ day2/django/setup.py
@@ -3,11 +3,13 @@ from setuptools import setup
 setup(
     name="django_blog",
     version="0.1.0",
-    packages=["djblog", "blog"],
+    packages=["djblog", "blog", "templates"],
+    include_package_data=True,
     install_requires=[
         "django",
         "django-extensions",
         "ipython",
         "django-markdownify",
+        "dynaconf",
     ],
 )
